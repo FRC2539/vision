7a0e742f7aa9986eef15706d54b9f2a1799e69ae
Krypton Cougars <tk1@kryptoncougars.pasd.us>
2017-02-04 14:41:20 +0000

    Whitespace change

--- mjpgFilter.cpp
+++ mjpgFilter.cpp
@@ -16,9 +16,9 @@ extern "C" {
 }
 
 struct Target {
-	double error;
-	int width;
-	int position;
+    double error;
+    int width;
+    int position;
 };
 
 // Globals are better than passing by pointer-to-pointer
@@ -47,19 +47,19 @@ bool filter_init(const char * args, void** filter_ctx) {
 void filter_process(void* filter_ctx, cv::Mat &src, cv::Mat &dst) {
     dst=src;
 
-	cv::Mat image;
-	cv::cvtColor(src, image, CV_BGR2HSV);
-	cv::inRange(image, lowerHSVthreshold, upperHSVthreshold, image);
+    cv::Mat image;
+    cv::cvtColor(src, image, CV_BGR2HSV);
+    cv::inRange(image, lowerHSVthreshold, upperHSVthreshold, image);
 
-	std::vector<std::vector<cv::Point>> contours;
-	std::vector<cv::Vec4i> hierarchy;
-	cv::findContours(
-		image,
-		contours,
-		hierarchy,
-		CV_RETR_TREE,
-		CV_CHAIN_APPROX_SIMPLE
-	);
+    std::vector<std::vector<cv::Point>> contours;
+    std::vector<cv::Vec4i> hierarchy;
+    cv::findContours(
+        image,
+        contours,
+        hierarchy,
+        CV_RETR_TREE,
+        CV_CHAIN_APPROX_SIMPLE
+    );
 
     cv::Scalar green(0, 255, 0);
     cv::drawContours(dst, contours, -1, green);
@@ -69,57 +69,57 @@ void filter_process(void* filter_ctx, cv::Mat &src, cv::Mat &dst) {
 
 void findTower(cv::Mat &output, std::vector<std::vector<cv::Point>> contours)
 {
-	std::vector<Target> targets;
-	for (auto contour : contours)
-	{
-		std::vector<cv::Point> hull;
-		cv::convexHull(cv::Mat(contour), hull);
-		if (cv::contourArea(hull) <= 200)
-		{
-			continue;
-		}
+    std::vector<Target> targets;
+    for (auto contour : contours)
+    {
+        std::vector<cv::Point> hull;
+        cv::convexHull(cv::Mat(contour), hull);
+        if (cv::contourArea(hull) <= 200)
+        {
+            continue;
+        }
 
-		cv::RotatedRect box = cv::minAreaRect(contour);
+        cv::RotatedRect box = cv::minAreaRect(contour);
 
-		double aspectRatio = box.size.height / box.size.width;
-		double error = std::abs(targetAspect - aspectRatio);
-		if (error > 0.4)
-		{
-			continue;
-		}
+        double aspectRatio = box.size.height / box.size.width;
+        double error = std::abs(targetAspect - aspectRatio);
+        if (error > 0.4)
+        {
+            continue;
+        }
 
-		Target currentTarget = Target();
-		currentTarget.width = box.size.width;
-		currentTarget.position = box.center.x - image.cols / 2.0;
-		currentTarget.error = error;
+        Target currentTarget = Target();
+        currentTarget.width = box.size.width;
+        currentTarget.position = box.center.x - image.cols / 2.0;
+        currentTarget.error = error;
 
-		targets.push_back(currentTarget);
-	}
+        targets.push_back(currentTarget);
+    }
 
-	if (targets.size() == 0)
-	{
-		targetInfo->PutBoolean("hasTarget", false);
-		return;
-	}
+    if (targets.size() == 0)
+    {
+        targetInfo->PutBoolean("hasTarget", false);
+        return;
+    }
 
-	int bestIndex = 0;
-	int bestError = 1;
-	int i = 0;
-	for (auto target : targets)
-	{
-		if (target.error < bestError)
-		{
-			bestIndex = i;
-		}
-		i++;
-	}
+    int bestIndex = 0;
+    int bestError = 1;
+    int i = 0;
+    for (auto target : targets)
+    {
+        if (target.error < bestError)
+        {
+            bestIndex = i;
+        }
+        i++;
+    }
 
-	targetInfo->PutBoolean("hasTarget", true);
-	targetInfo->PutNumber("centerX", targets[bestIndex].position);
-	targetInfo->PutNumber(
-		"distance",
-		22.837262 - 0.210646 * targets[bestIndex].width
-	)
+    targetInfo->PutBoolean("hasTarget", true);
+    targetInfo->PutNumber("centerX", targets[bestIndex].position);
+    targetInfo->PutNumber(
+        "distance",
+        22.837262 - 0.210646 * targets[bestIndex].width
+    )
 }
 
 void findLift(cv::Mat &output, std::vector<std::vector<cv::Point>> contours)
